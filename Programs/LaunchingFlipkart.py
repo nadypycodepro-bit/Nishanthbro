9afd1dd363178a89529fdd864e6b4c41d84de452
Nadim <nadim@gmail.com>
2026-05-26 14:52:08 +0530

    first commit

---
+++ Programs/LaunchingFlipkart.py
@@ -0,0 +1,15 @@
+from selenium.webdriver import Chrome,ChromeOptions
+from time import sleep
+
+opts = ChromeOptions()
+opts.add_experimental_option("detach",True)
+
+
+driver = Chrome(opts)
+driver.get("https://www.flipkart.com/")
+driver.maximize_window()
+sleep(3)
+search = driver.find_element("name","q")
+search.send_keys("Iphone")
+search.submit()
+print("Successfully Launched")
