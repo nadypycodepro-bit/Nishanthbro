78ebcef55b1dacda2a1eb2f545bcd0f19b099fe3
Nadim <nadim@gmail.com>
2026-05-26 16:41:23 +0530

    New test

--- Programs/test_LaunchingFlipkart.py
+++ Programs/test_LaunchingFlipkart.py
@@ -5,14 +5,14 @@ opts = ChromeOptions()
 opts.add_experimental_option("detach",True)
 opts.add_argument("--start-maximized")
 
-
-driver = Chrome(opts)
-driver.get("https://www.flipkart.com/")
-# driver.maximize_window()
-sleep(3)
-search = driver.find_element("name","q")
-search.send_keys("Iphone")
-search.submit()
-print("Successfully Launched")
-sleep(3)
-driver.close() # Used to close the browser
+def test_TC1():
+    driver = Chrome(opts)
+    driver.get("https://www.flipkart.com/")
+    # driver.maximize_window()
+    sleep(3)
+    search = driver.find_element("name","q")
+    search.send_keys("Iphone")
+    search.submit()
+    print("Successfully Launched")
+    sleep(3)
+    driver.close() # Used to close the browser
